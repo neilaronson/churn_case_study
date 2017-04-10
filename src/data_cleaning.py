54405ccc13eba77d031bbf741720c32459eea227
neilaronson <neilaronson@gmail.com>
2017-04-09 19:00:03 -0700

    upgraded data cleaning pipeline

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -15,10 +15,9 @@ class DataCleaning(object):
         self.df['Churn'] = (self.df.last_trip_date < '2014-06-01').astype(int)
 
     def dummify(self, columns):
-        dummies = pd.get_dummies(self.df[columns], prefix = columns)
-        self.df = self.df.drop(columns, axis = 1)
-        # return pd.concat([df,dummies], axis = 1)
-        self.df = pd.concat([self.df,dummies], axis = 1)
+        dummies = pd.get_dummies(self.df[columns], prefix=columns)
+        self.df = self.df.drop(columns, axis=1)
+        self.df = pd.concat([self.df,dummies], axis=1)
 
     def drop_date_columns(self):
         self.df.drop('last_trip_date', axis=1, inplace=True)
@@ -28,10 +27,10 @@ class DataCleaning(object):
         return list(self.df.columns.values)
 
     def cut_outliers(self, col):
-       std = self.df[col].std()
-       t_min = self.df[col].mean() - 3*std
-       t_max = self.df[col].mean() + 3*std
-       self.df = self.df[(self.df[col] >= t_min) & (self.df[col] <= t_max)]
+        std = self.df[col].std()
+        t_min = self.df[col].mean() - 3*std
+        t_max = self.df[col].mean() + 3*std
+        self.df = self.df[(self.df[col] >= t_min) & (self.df[col] <= t_max)]
 
     def total_distance(self):
         self.df['total_distance'] = self.df['avg_dist'] * self.df['trips_in_first_30_days']
@@ -43,38 +42,46 @@ class DataCleaning(object):
         self.df = self.df.drop(['avg_dist', 'trips_in_first_30_days'], axis=1)
 
     def make_log_no_trips(self):
-        #import ipdb; ipdb.set_trace()
         self.df['log_trips'] = self.df[(self.df['trips_in_first_30_days'] != 0)].trips_in_first_30_days.apply(np.log)
         self.df['log_trips'] = self.df['log_trips'].apply(lambda x: 0 if np.isnan(x) else x)
-        #self.df["trips_in_first_30_days"] = np.log(self.df["trips_in_first_30_days"])+1
 
-    def drop_low_log_columns(self):
+    def drop_columns_for_regression(self):
         self.df = self.df.drop(['phone_iPhone', 'city_Astapor'], axis=1)
 
-    def clean(self):
+    def mark_missing(self, cols):
+        for col in cols:
+            self.df[col].fillna('missing', inplace=True)
+
+    def make_rating_dummies(self):
+        self.df['5star_driver'] = (self.df.avg_rating_of_driver==5.)
+        self.df['missing_dr_rating'] = (self.df.avg_rating_of_driver=='missing')
+        self.df['non5star_driver'] = (self.df.avg_rating_of_driver!='missing') & (self.df.avg_rating_of_driver!=5.)
+        self.df = self.df.drop('avg_rating_of_driver', axis=1)
+
+    def clean(self, regression=False):
         self.make_target_variable()
+        self.mark_missing(['avg_rating_of_driver'])
+        self.drop_na()
+        self.make_rating_dummies()
         self.drop_date_columns()
 
         # making total distance
         # self.total_distance()
         # self.drop_avg_dist_and_trips()
 
-        #self.make_log_no_trips()
-
         for column in ['avg_dist', 'trips_in_first_30_days', 'surge_pct']:
            self.cut_outliers(column)
 
         for column in ['city', 'phone']:
             self.dummify(column)
-        #self.drop_low_log_columns()
-        self.drop_na()
+
         y = self.df.pop('Churn')
 
-        #drop low value coeffs
-
-
-        ss = StandardScaler()
-
         X = self.df.values
-        X = ss.fit_transform(X)
+
+        if regression:
+            self.drop_columns_for_regression()
+            ss = StandardScaler()
+            X = ss.fit_transform(X)
+
         return X, y
